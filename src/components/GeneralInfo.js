892c8999325106cc0f6ce71e12e5b884d34e18c2
Hamish <hamishok@gmail.com>
2023-06-23 15:12:36 +1200

    State updates to parent from child in one component

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,11 +3,12 @@ import React from 'react';
 class GeneralInfo extends React.Component {
     constructor(props){
         super(props);
-        this.onChange =this.onChange.bind(this);
+        // this.onChange =this.onChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
     }
 
-    onChange(event) {
-        this.setState({[event.target.name]: event.target.value})
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
     }
 
     handleClick(e) {
@@ -16,16 +17,17 @@ class GeneralInfo extends React.Component {
     }
 
     render() {
-        const { name, email, phone } = this.props;
+        const { userName, userEmail, userPhone } = this.props;
+        console.log(userName)
         return(
             <>
                 <form label="General Info" preventDefault>
-                    <input type = "text" placeholder="Name" name="name" value={name} onChange={this.onChange} />
+                    <input type = "text" placeholder="Name" name="userName" value={userName} onChange={this.handleNameChange} />
                     <br/>
-                    <input type = "text" placeholder="Email" name="email" value={email} onChange={this.onChange} />
+                    {/* <input type = "text" placeholder="Email" name="userEmail" value={userEmail} onChange={this.onChange} />
                     <br/>
-                    <input type="text" placeholder="Phone Number" name="phone" value={phone} onChange={this.onChange} />
-                    <br/>
+                    <input type="text" placeholder="Phone Number" name="userPhone" value={userPhone} onChange={this.onChange} />
+                    <br/> */}
                     <button onClick={ (e) => this.handleClick(e)}>Submit</button>
                 </form>
                 
--- src/components/PageDisplay.js
+++ src/components/PageDisplay.js
@@ -4,17 +4,25 @@ import EducationalExperience from './EducationalExperience';
 import WorkExperience from './WorkExperience';
 
 class PageDisplay extends React.Component {
-    state = {
-        companyName: '',
-        positionTitle: '',
-        mainTasks: '',
-        datesWorked: '',
-        name: '',
-        email: '',
-        phone: '',
-        schoolName: '',
-        titleOfStudy: '',
-        dateOfStudy: ''
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.state = {
+            companyName: '',
+            positionTitle: '',
+            mainTasks: '',
+            datesWorked: '',
+            name: 'wow',
+            email: '',
+            phone: '',
+            schoolName: '',
+            titleOfStudy: '',
+            dateOfStudy: ''
+        }
+    }
+    
+    handleNameChange(userName){
+        this.setState({name: userName});
     }
 
     render() {
@@ -23,11 +31,16 @@ class PageDisplay extends React.Component {
             <div>
                 <h1>Curriculum Vitae</h1>
                 <h3>General Info</h3>
-                <GeneralInfo/>
+                <GeneralInfo userName={name} userEmail={email} userPhone={phone} onNameChange={this.handleNameChange}/>
                 <h3>Educational Experience</h3>
                 <EducationalExperience/>
                 <h3>Work Experience</h3>
                 <WorkExperience/>
+                <h1>Curriculum Vitae</h1>
+                <h3>General Info</h3>
+                <p>{this.state.name}</p>
+                <p>{this.state.email}</p>
+                <p>{this.state.phone}</p>
             </div>
         )
     }
